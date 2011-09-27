d8b67ed01968374baf88936fae24283665873c7d
rabeesh <rabeesh@oysis.com>
2011-09-27 12:46:05 +0000

    git-svn-id: http://svn.binnyva.com/stockerboy/trunk@5 6077151e-888b-45d7-9275-6c6bdddffbf7

--- system/application/models/product_model.php
+++ system/application/models/product_model.php
@@ -16,17 +16,20 @@ class Product_model extends Model {
     function Product_model() {
         parent::Model();
         $this->ci = &get_instance();
+       //$this->city_id = $this->ci->session->userdata('city_id');
+        //$this->project_id = $this->ci->session->userdata('project_id');
     }
 	
 	/*
 			Function to add product type
 	*/
+	
 	function addproducttype($data)
 	{
 		$ptype  = trim($data['ptype']);
 		
 		$this->db->select('*');
-        $this->db->from('Product');
+        $this->db->from('product');
         $this->db->where('name',$ptype);
         $result = $this->db->get();
 		
@@ -36,7 +39,7 @@ class Product_model extends Model {
 								 );
 								   
                 $this->db->set($ptypeInfo);
-                $this->db->insert('Product');
+                $this->db->insert('product');
 				                
 				return ($this->db->affected_rows() > 0) ? $this->db->insert_id(): false ;
 	
@@ -45,6 +48,44 @@ class Product_model extends Model {
             {
                 return 'producttype_already_taken';
             }
+	}
+	
+	function adddesign($data)
+	{
+		$ptype  = trim($data['ptype']);
+		$dname  = trim($data['dname']);
+		$img  = trim($data['img']);
+		
+		$this->db->select('*');
+        $this->db->from('design');
+        $this->db->where('name',$dname);
+        $result = $this->db->get();
+		
+        	if($result->num_rows() == 0) 
+            {
+                $designInfo = array( 'name'  => $dname,
+									'product_id'  => $ptype,
+									'img_name'  => $img
+								 );
+								   
+                $this->db->set($designInfo);
+                $this->db->insert('design');
+				                
+				return ($this->db->affected_rows() > 0) ? $this->db->insert_id(): false ;
+	
+            }
+            else
+            {
+                return 'designname_already_taken';
+            }
+	}
+	
+	function get_producttype()
+	{
+		$this->db->select('*');
+        $this->db->from('product');
+        $result = $this->db->get();
+		return $result;
 	}
 	
 }
