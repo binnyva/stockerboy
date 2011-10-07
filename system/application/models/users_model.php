97d30fe44ae724f6ee63c1566150dcd1087d4c0e
binnyva <binnyva@gmail.com>
2011-10-07 15:37:10 +0000

    Updates
    git-svn-id: http://svn.binnyva.com/stockerboy/trunk@15 6077151e-888b-45d7-9275-6c6bdddffbf7

--- system/application/models/users_model.php
+++ system/application/models/users_model.php
@@ -31,7 +31,7 @@ class Users_model extends Model {
       	$username= $data['username'];
         $password = $data['password'];
 		
-		$query = $this->db->where('email', $username)->where('password',$password)->where('status','1')->get("User");
+		$query = $this->db->where('email', $username)->where('password',$password)->where('status','1')->get("user");
         if($query->num_rows() > 0) {
 			$user = $query->first_row();
    			$memberCredentials['id'] = $user->id;
@@ -50,39 +50,29 @@ class Users_model extends Model {
 	
 	
 	function search_users($data) {
-		$this->db->select('User.id,User.name,User.email,User.password,User.phone, City.name as city_name');
-		$this->db->from('User');
-		$this->db->join('City', 'City.id = User.city_id' ,'left');
-		
+		$this->db->select('user.id,user.name,user.email,user.password,user.phone, city.name as city_name');
+		$this->db->from('user');
+		$this->db->join('city', 'city.id = user.city_id' ,'left');
 		
 		if(!isset($data['status'])) $data['status'] = 1;
-		if($data['status'] !== false) $this->db->where('User.status', $data['status']); // Setting status as 'false' gets you even the deleted users
+		if($data['status'] !== false) $this->db->where('user.status', $data['status']); // Setting status as 'false' gets you even the deleted users
 		
 		
-		if(isset($data['city_id']) and $data['city_id'] != 0) $this->db->where('User.city_id', $data['city_id']);
-		else if(!isset($data['city_id'])) $this->db->where('User.city_id', $this->city_id);
+		if(isset($data['city_id']) and $data['city_id'] != 0) $this->db->where('user.city_id', $data['city_id']);
+		else if(!isset($data['city_id'])) $this->db->where('user.city_id', $this->city_id);
 		
-		if(!empty($data['name'])) $this->db->like('User.name', $data['name']);
-		if(!empty($data['phone'])) $this->db->where('User.phone', $data['phone']);
-		if(!empty($data['email'])) $this->db->where('User.email', $data['email']);
+		if(!empty($data['name'])) $this->db->like('user.name', $data['name']);
+		if(!empty($data['phone'])) $this->db->where('user.phone', $data['phone']);
+		if(!empty($data['email'])) $this->db->where('user.email', $data['email']);
 		
-		
-		$this->db->orderby('User.name');
+		$this->db->orderby('user.name');
 		
 		$all_users = $this->db->get()->result();
-		//echo $this->db->last_query();
-
-		$return = array();
-		/*foreach($all_users as $user) {
-			// Get the batches for this User. An user can have two batches. That's why I don't do join to get this date.
-			//$user->batches = colFormat($this->db->where('user_id',$user->id)->get('UserBatch')->result_array()); // :SLOW:
-			
-			// Gets the UserGroup of the users...
-			if(!empty($data['get_user_groups'])) $user->groups = $this->get_user_groups_of_user($user->id);
-			
-			$return[$user->id] = $user;
-		}*/
-		return $return;
+		
+		return $all_users;
 	}
-
+	
+	function get_users_city($user_id) {
+		return $this->db->select('city_id')->from('user')->where(array('id'=>$user_id))->get()->row()->city_id;
+	}
 }
